49487100e0c5d85ec3d8f4b1e0106ae900c0f704
Csabi <simicsabi1998@gmail.com>
2020-04-08 18:02:44 +0200

    security - UserDetailsServiceImpl

---
+++ backend/src/main/java/hu/elte/Tanart_Keres_Kinal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package hu.elte.Tanart_Keres_Kinal.security;
+
+import hu.elte.Tanart_Keres_Kinal.entities.User;
+import hu.elte.Tanart_Keres_Kinal.repositories.UserRepository;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired 
+    private AuthenticatedUser authenticatedUser;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> oUser = userRepository.findByUserName(username);
+        if (!oUser.isPresent()) {
+            throw new UsernameNotFoundException(username);
+        }
+        User user = oUser.get();
+        authenticatedUser.setUser(user);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(user.getStatus().toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), grantedAuthorities);
+    }
+}
