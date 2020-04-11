fb606c071731edcc10ddbdcadc97a9ed3b53497f
curlygabika <56161625+curlygabika@users.noreply.github.com>
2020-04-11 20:35:24 +0200

    final bat files

--- allinone.bat
+++ allinone.bat
@@ -1,4 +1,6 @@
-(
-start backend.bat
-start frontend.bat
-) | pause
+call git clone https://github.com/curlygabika/tanart_keres_kinal.git
+cd tanart_keres_kinal
+
+start "" backend.bat
+start "" frontend.bat
+cmd explorer "http://localhost:4200/"
--- frontend.bat
+++ frontend.bat
@@ -1,4 +1,3 @@
 call cd frontend
 call npm install 
 call npm start
-call cmd explorer "http://localhost:4200/"
