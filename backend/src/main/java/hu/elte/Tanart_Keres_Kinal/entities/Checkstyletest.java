52cb45e225cc9ac1c33784dcb8abc94717451d15
curlygabika <56161625+curlygabika@users.noreply.github.com>
2020-04-25 11:57:04 +0200

    Create Checkstyletest.java

---
+++ backend/src/main/java/hu/elte/Tanart_Keres_Kinal/entities/Checkstyletest.java
@@ -0,0 +1,12 @@
+/*
+this is just a test for checkstyle
+ */
+package hu.elte.alkfejl.ClassTracker.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+public class Checkstyletest {
+
+}
